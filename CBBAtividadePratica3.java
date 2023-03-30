916e9120d79fd7d7cfa7309714cc6fb6712d6b21
Eduardo Whitehurst <119756830+eduardo-whitehurst@users.noreply.github.com>
2023-03-29 21:04:52 -0300

    Upload das questoes CBB2, CBB3 e CBB4 e BM4

---
+++ CBBAtividadePratica3.java
@@ -0,0 +1,11 @@
+public class CBBAtividadePratica3 {   
+    public static void main(String[] args) {
+        
+        double base = 2.5;
+        double altura = 5.5;
+
+        double area = base * altura;
+
+        System.out.println("A area do retangulo eh: " + area);
+    }
+}
